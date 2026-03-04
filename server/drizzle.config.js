a313f88dc6e0ddc7da203d698df4a63f9b8e5f3b
kvilmos <kvilmos47@gmail.com>
2026-03-04 12:52:04 +0000

    refactor: move database connection to the db directory

--- server/drizzle.config.js
+++ server/drizzle.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
+  out: "./src/db/migrations",
   schema: "./src/db/schema.js",
   dialect: "postgresql",
+  casing: "snake_case",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
