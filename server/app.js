e4b8ce7b7deb95f7eab11bdfaa5690c35f59a76d
kvilmos <kvilmos47@gmail.com>
2026-03-05 11:25:22 +0000

    feat: add email verification implementing Mailgun service

--- server/app.js
+++ server/app.js
@@ -14,9 +14,8 @@ app
   .use("*", logger())
   .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
   .route("/api/v1", index)
-  .route("/api/v1/users", users);
-
-app.notFound(notFound);
-app.onError(error);
+  .route("/api/v1/users", users)
+  .notFound(notFound)
+  .onError(error);
 
 export default app;
