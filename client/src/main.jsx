26f6d888c7ab9640b772d71818044100c2d2b0f4
kvilmos <kvilmos47@gmail.com>
2026-03-05 14:29:52 +0000

    feat: add shadcn ui library

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import router from "./app/router/router";
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
