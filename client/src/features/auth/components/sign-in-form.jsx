02e10da13f8f54c72e2e8c0453e04434c2ff7b2d
kvilmos <kvilmos47@gmail.com>
2026-03-08 16:19:55 +0000

    feat: add sign-in functionality

--- client/src/features/auth/components/sign-in-form.jsx
+++ client/src/features/auth/components/sign-in-form.jsx
@@ -2,32 +2,117 @@ import { Button } from "@/components/shadcn-ui/button";
 import { Input } from "@/components/shadcn-ui/input";
 
 import "@/features/auth/auth-page.css";
-import { Field, FieldGroup, FieldLabel } from "@/components/shadcn-ui/field";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/shadcn-ui/field";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signInSchema } from "@app/shared/schemas/auth-schema";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { Spinner } from "@/components/shadcn-ui/spinner";
+
+import { toast } from "sonner";
 
 export default function SignInForm({ onSwitch }) {
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onSubmit(values) {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+        rememberMe: true,
+      },
+      {
+        onRequest: () => {
+          setIsLoading(true);
+        },
+        onSuccess: () => {
+          toast("Successful log in.", {
+            position: "top-center",
+          });
+        },
+        onError: (ctx) => {
+          console.log(ctx);
+          toast.warning(ctx.error.message, { position: "top-center" });
+          form.setError("root", { message: ctx.error.message });
+        },
+      },
+    );
+
+    setIsLoading(false);
+  }
+
   return (
     <div>
       <h1 className="text-xl">Welcome Back</h1>
       <p className="text-sm">Please enter your details.</p>
+      <form id="sign-in-form" onSubmit={form.handleSubmit(onSubmit)} noValidate>
+        <FieldGroup className="auth-form">
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="auth-field">
+                <FieldLabel htmlFor="sign-in-email">Email</FieldLabel>
+                <Input
+                  {...field}
+                  id="sign-in-email"
+                  type="email"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="example@mail.com"
+                  autoComplete="email"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          ></Controller>
 
-      <FieldGroup className="auth-form">
-        <Field className="auth-field">
-          <FieldLabel htmlFor="signin-email">Email</FieldLabel>
-          <Input
-            id="signin-email"
-            type="email"
-            placeholder="example@mail.com"
-          />
-        </Field>
-        <Field className="auth-field">
-          <FieldLabel htmlFor="signin-password">Password</FieldLabel>
-          <Input id="signin-password" type="password" />
-        </Field>
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="auth-field">
+                <FieldLabel htmlFor="sign-in-password">Password</FieldLabel>
+                <Input
+                  {...field}
+                  id="sign-in-password"
+                  type="password"
+                  aria-invalid={fieldState.invalid}
+                  autoComplete="password"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          ></Controller>
 
-        <Field>
-          <Button type="submit">Sign In</Button>
-        </Field>
-      </FieldGroup>
+          <Field>
+            <Button
+              form="sign-in-form"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
+              {isLoading ? <Spinner></Spinner> : "Sign In"}
+            </Button>
+          </Field>
+        </FieldGroup>
+      </form>
 
       <p className="text-sm switch-p">
         Don't have an account?&nbsp;
--- shared/schemas/auth-schema.js
+++ shared/schemas/auth-schema.js
@@ -3,13 +3,14 @@ import z from "zod";
 export const signUpSchema = z
   .object({
     email: z
-      .email("Invalid email address")
+      .email("Please enter a valid email address")
       .trim()
       .min(5, "Email must be at least 5 characters")
       .max(100, "Email must be less than 100 characters"),
     fullName: z
       .string()
       .trim()
+      .nonempty("Please enter your full name")
       .regex(/^[\p{L}\s]+$/u, {
         message: "Only letters and spaces are allowed",
       })
@@ -18,11 +19,24 @@ export const signUpSchema = z
     password: z
       .string()
       .trim()
+      .nonempty("Password cannot be empty")
       .min(8, "Password must be at least 6 characters")
       .max(100, "Password must be less than 100 characters"),
-    confirmPassword: z.string().trim().nonempty("Confirm the password"),
+    confirmPassword: z.string().trim().nonempty("Please confirm your password"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
+export const signInSchema = z.object({
+  email: z
+    .email("Please enter a valid email address")
+    .trim()
+    .max(100, "Email must be less than 100 characters"),
+  password: z
+    .string()
+    .trim()
+    .nonempty("Please enter your password")
+    .max(100, "Password must be less than 100 characters"),
+});
